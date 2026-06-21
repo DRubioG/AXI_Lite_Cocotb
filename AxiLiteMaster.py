481791e6263ea740f4ceb06442616588b3d8693f
DRubioG <davidrubio97@outlook.com>
2026-06-21 17:46:43 +0200

    update

--- AxiLiteMaster.py
+++ AxiLiteMaster.py
@@ -1,14 +1,20 @@
 import cocotb
-from cocotb.triggers import RisingEdge, Timer
+from cocotb.triggers import RisingEdge
 
 
 class AxiLiteMaster:
+    """
+    Class to test the AXI Lite as a Master.
+    """
     def __init__(self, dut, clk, prefix="s00_axi"):
+        """
+        Predefine the value.
+        """
         self.dut = dut
         self.clk = clk
         self.p = prefix
 
-        # Inicialización AXI-Lite (evita X y problemas al arrancar)
+        # Initialice the signals.
         self._s("awvalid").value = 0
         self._s("wvalid").value  = 0
         self._s("bready").value  = 0
@@ -16,105 +22,95 @@ class AxiLiteMaster:
         self._s("rready").value  = 0
 
     def _s(self, name):
-        return getattr(self.dut, f"{self.p}_{name}")
-
-    def _s(self, name):
+        """Get the port as Python signal.
+        """
         return getattr(self.dut, f"{self.p}_{name}")
 
     async def wait_ready(self, sig, timeout=1000):
+        """Waiting to avoid blocking.
+
+        Args:
+            sig (int): condition
+            timeout (int, optional): Value to wait. Defaults to 1000.
+
+        Raises:
+            cocotb.result.TestFailure: Timeout waiting.
+        """
         for _ in range(timeout):
             if sig.value:
                 return
             await RisingEdge(self.clk)
-        raise cocotb.result.TestFailure(f"Timeout esperando {sig._name}")
+        raise cocotb.result.TestFailure(f"Timeout waiting {sig._name}")
 
 
     async def write(self, addr, data: bytes):
-        self.dut._log.warning("paso 1")
+        """This method writes in the register.
+
+        Args:
+            addr (int): address to write.
+            data (bytes): data to write.
+        """
         self._s("awaddr").value = addr
         self._s("wdata").value = int.from_bytes(data, "little")
         self._s("wstrb").value = (1 << len(data)) - 1
 
-        self.dut._log.warning("paso 2")
-        # Activar AWVALID y WVALID a la vez
+
+        # Active AWVALID and WVALID at the same time.
         self._s("awvalid").value = 1
         self._s("wvalid").value  = 1
 
-        self.dut._log.warning("paso 3")
-        # Esperar a que el DUT acepte dirección + datos
+
+        # Wait unitl the DUT accepts the address and data.
         await self.wait_ready(self._s("awready"))
 
 
-        self.dut._log.warning("paso 4")
-        # Desactivar ambos
+        # Deactivate both
         self._s("awvalid").value = 0
         self._s("wvalid").value  = 0
 
 
 
         # B channel
-        self.dut._log.warning("paso 5")
+        
         self._s("bready").value = 1
         await self.wait_ready(self._s("bvalid"))
         self._s("bready").value = 0
 
 
     async def read(self, addr, size):
-        # AR channel
+        """This method reads the register value.
+
+        Args:
+            addr (int): address to read
+            size (int): bytes to read
+
+        Returns:
+            int: read value 
+        """
+        # --- AR channel ---
         self._s("araddr").value = addr
         self._s("arvalid").value = 1
-        await self.wait_ready(self._s("arready"))
-        self._s("arvalid").value = 0
 
-        # R channel
+        # Wait until the slave accept the address.
+        await self.wait_ready(self._s("arready"))
+
+        # Drops ARVALID to complete the handshake
+        self._s("arvalid").value = 0
+        self._s("araddr").value = 0
+
+        # --- R channel ---
         self._s("rready").value = 1
+
+        # Wait until the data is available.
         await self.wait_ready(self._s("rvalid"))
+
+        # Captures the data.
         val = int(self._s("rdata").value)
+
+        # Drops the RREADY to complete the handshake.
         self._s("rready").value = 0
 
+
+        # Return the value in bytes format.
         return val.to_bytes(size, "little")
-
-
-
-async def clock_gen(clk, period_ns=1):
-    # await RisingEdge(clk)
-    while True:
-        clk.value = 0
-        await Timer(period_ns/2, units="ns")
-        clk.value = 1
-        await Timer(period_ns/2, units="ns")
-
-
-async def reset_gen(rst, clk, cycles=5):
-    rst.value = 0
-    for _ in range(cycles):
-        await RisingEdge(clk)
-    rst.value = 1
-    await RisingEdge(clk)
-
-
-@cocotb.test()
-async def test_axi_lite(dut):
-
-    # Lanzar reloj en paralelo
-    axi = AxiLiteMaster(dut, dut.s00_axi_aclk, prefix="s00_axi")
-    cocotb.start_soon(clock_gen(dut.s00_axi_aclk))
-
-    # Reset
-    await reset_gen(dut.s00_axi_aresetn, dut.s00_axi_aclk)
-
-    for _ in range(15):
-        await RisingEdge(dut.s00_axi_aclk)
-    # Escritura
-    await axi.write(0x00, b"\x01\x00\x00\x00")
-
-    await axi.write(0x04, (127).to_bytes(4, "little"))
-    # Lectura
-    data = await axi.read(0x00, 4)
-    dut._log.info(f"Read back: {data.hex()}")
-
-    for _ in range(5000000):
-        await RisingEdge(dut.s00_axi_aclk)
-        t = cocotb.utils.get_sim_time()
-        dut._log.info(f"Tiempo sim: {t}")
 
--- README.md
+++ README.md
@@ -1,3 +1,52 @@
 # Axi-Lite Cocotb
 
 This tests the AXI-Lite protocol
+
+``` python
+
+import cocotb
+from cocotb.triggers import RisingEdge, Timer
+from AxiLiteMaster import *
+
+async def clock_gen(clk, period_ns=1):
+    # await RisingEdge(clk)
+    while True:
+        clk.value = 0
+        await Timer(period_ns/2, units="ns")
+        clk.value = 1
+        await Timer(period_ns/2, units="ns")
+
+
+async def reset_gen(rst, clk, cycles=5):
+    rst.value = 0
+    for _ in range(cycles):
+        await RisingEdge(clk)
+    rst.value = 1
+    await RisingEdge(clk)
+
+
+@cocotb.test()
+async def test_axi_lite(dut):
+
+    # Launch the parallel clock
+    axi = AxiLiteMaster(dut, dut.s00_axi_aclk, prefix="s00_axi")
+    cocotb.start_soon(clock_gen(dut.s00_axi_aclk))
+
+    # Reset
+    await reset_gen(dut.s00_axi_aresetn, dut.s00_axi_aclk)
+
+    for _ in range(15):
+        await RisingEdge(dut.s00_axi_aclk)
+    # Write
+    await axi.write(0x00, b"\x01\x00\x00\x00")
+
+    await axi.write(0x04, (127).to_bytes(4, "little"))
+    # Read
+    data = await axi.read(0x00, 4)
+    dut._log.info(f"Read back: {data.hex()}")
+
+    for _ in range(5000000):
+        await RisingEdge(dut.s00_axi_aclk)
+        t = cocotb.utils.get_sim_time()
+        dut._log.info(f"Tiempo sim: {t}")
+```
